a7fc0b5743b8624e51496a1d6fa0cd32d18f3e44
Romain Piel <romain.piel@gmail.com>
2012-11-07 16:33:22 +0100

    init require.js components

---
+++ public/javascripts/main.js
@@ -0,0 +1,22 @@
+requirejs.config({
+    paths: {
+        jquery: 'jquery-1.8.2.min'
+    }
+});
+
+
+// Load the application.
+require(
+    [
+        "jquery",
+        "arbor",
+        "arbor-tween"
+    ],
+    function($){
+
+
+        
+
+
+    }
+);
