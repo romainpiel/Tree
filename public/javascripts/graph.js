f06f077ccb0ca402a72631308b0721f58b7a898e
Romain Piel <romain.piel@gmail.com>
2012-11-09 23:09:06 +0100

    a few settings

--- public/javascripts/graph.js
+++ public/javascripts/graph.js
@@ -11,6 +11,22 @@ define([
             init: function() {
                 var sigRoot = document.getElementById('sig');
                 this.sigInst = sigma.init(sigRoot);
+
+                this.sigInst.drawingProperties({
+                    defaultLabelColor: '#fff',
+                    defaultLabelSize: 14,
+                    defaultLabelBGColor: '#fff',
+                    defaultLabelHoverColor: '#000',
+                    labelThreshold: 6,
+                    defaultEdgeType: 'curve'
+                }).graphProperties({
+                    minNodeSize: 0.5,
+                    maxNodeSize: 1,
+                    minEdgeSize: 1,
+                    maxEdgeSize: 1
+                }).mouseProperties({
+                    maxRatio: 32
+                });
             },
 
             add: function(username, users) {
