c4c3c41518eae57a5bce391d3943f688533e779e
Romain Piel <romain.piel@gmail.com>
2012-11-14 17:55:13 +0100

    added api to get user avatar

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,37 @@
-
 module.exports = function(app, isLoggedIn) {
 
     var appdotnet = require('../lib/appdotnet');
-    
+
     app.get('/', function(req, res) {
-        res.render('index', 
-            { 
-                title: "Tree"
-            });
+        res.render('index', {
+            title: "Tree"
+        });
     });
 
     app.get('/graph', function(req, res) {
-        if (req.session.passport.user) {
-            res.render('graph', 
-                { 
-                    title: "Tree",
-                });
+        if(req.session.passport.user) {
+            res.render('graph', {
+                title: "Tree",
+            });
         } else {
             res.redirect("/auth/appdotnet");
         }
     });
 
     app.get('/user', isLoggedIn, function(req, res) {
-        appdotnet.user(req, function(err, result) {
-          if (err) {
-            res.status(500);
-            res.json({ 'error': 'error retrieving user' });
-          } else {
-            res.json(result);
-          }
+        appdotnet.getUser(req, function(err, result) {
+            if(err) {
+                res.status(500);
+                res.json({
+                    'error': 'error retrieving user'
+                });
+            } else {
+                res.json(result);
+            }
         });
+    });
+
+    app.get('/user/avatar/:userid', function(req, res) {
+        res.redirect(appdotnet.getAvatarUrl(req));
     });
 }
